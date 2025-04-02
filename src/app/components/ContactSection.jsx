ea1ce93e6590e647d8fb8cd28195ffe2624c89b7
Ansar Ali <ansar@amharctech.com>
2025-04-02 15:50:16 +0600

    diamond section fixed

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -2,45 +2,47 @@ import { MdEmail, MdLocationOn, MdPhoneInTalk } from 'react-icons/md';
 
 export default function ContactSection() {
     return (
-        <section className="py-16 md:py-20 px-4 md:px-32 overflow-hidden">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-[15px] md:gap-0 items-center pb-[60px]">
+        <section className="py-[40px] lg:py-20 px-[20px] lg:px-32 overflow-hidden">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-[15px] lg:gap-0 items-center pb-[25px] lg:pb-[60px]">
                 {/* Column 1 */}
-                <div>
-                    <p className="text-[#ED7D4A] text-center md:text-left leading-trim md:leading-none text-edge-cap font-[inter] font-semibold tracking-[-0.02em] uppercase text-[22px] md:text-[40px] sm:text-[24px] md:sm:text-[40px]">
+                <div className="lg:col-span-5">
+                    <p className="text-[#ED7D4A] text-center lg:text-left leading-trim lg:leading-none text-edge-cap font-[inter] font-semibold tracking-[-0.02em] uppercase text-[22px] lg:text-[40px] sm:text-[24px] lg:sm:text-[40px]">
                         <span className="text-white">CONTACT</span> US
                     </p>
                 </div>
 
-                <div className="flex justify-center">
+                <div className="lg:flex justify-center hidden lg:col-span-2">
                     <div className="w-[1.5px] h-[81px] bg-[rgba(255,94,39,0.20)]"></div>
                 </div>
 
                 {/* Column 2 */}
-                <div>
-                    <p className="text-white font-poppins font-light text-sm leading-[156%] sm:text-base sm:leading-[156%]">Any question or remarks? Just write us a message</p>
+                <div className="text-center lg:col-span-5">
+                    <p className="text-white font-[poppins] font-light text-[14px] leading-[20px] sm:text-base sm:leading-[156%]">
+                        Any question or remarks? Just write us a message
+                    </p>
                 </div>
             </div>
-            <div className="border border-[#ED7D4A] p-[10px] rounded">
-                <div className=" grid grid-cols-1 md:grid-cols-12 gap-[114px]">
+            <div className="border border-[#ED7D4A] py-[15px] px-[8px] lg:p-[10px] rounded">
+                <div className=" grid grid-cols-1 lg:grid-cols-12 gap-[10px]  xl:gap-[114px]">
                     {/* Contact Information */}
 
-                    <div className="bg-[#ED7D4A] p-[40px] rounded col-span-5">
-                        <h3 className="text-white font-inter font-semibold text-[28px] leading-none mb-[16px]">Contact Information</h3>
+                    <div className="bg-[#ED7D4A] py-[40px] px-[12px] lg:p-[40px] rounded lg:col-span-5">
+                        <h3 className="text-white font-[inter] font-semibold text-[28px] leading-none mb-[16px]">Contact Information</h3>
                         <p className="text-white font-poppins font-normal text-[16px] leading-normal pb-[55px]">Say something to start a live chat!</p>
                         <div className="space-y-[55px]">
-                            <p className="flex items-center text-white font-inter font-normal text-[16px] leading-normal">
+                            <p className="flex items-center text-white font-[inter] font-normal text-[16px] leading-normal">
                                 <span className="pr-6">
                                     <MdPhoneInTalk className="text-white" />
                                 </span>
                                 +331 54 296 7490
                             </p>
-                            <p className="flex items-center text-white font-inter font-normal text-[16px] leading-normal">
+                            <p className="flex items-center text-white font-[inter] font-normal text-[16px] leading-normal">
                                 <span className="pr-6">
                                     <MdEmail className="text-white" />
                                 </span>
                                 mondher.saadaoui@gmail.com
                             </p>
-                            <p className="flex items-center text-white font-inter font-normal text-[16px] leading-normal">
+                            <p className="flex items-center text-white font-[inter] font-normal text-[16px] leading-normal">
                                 <span className="pr-6">
                                     <MdLocationOn className="text-white" />
                                 </span>
@@ -50,11 +52,11 @@ export default function ContactSection() {
                     </div>
 
                     {/* Contact Form */}
-                    <div className="col-span-7 my-[20px] mx-[15px]">
+                    <div className="lg:col-span-7  my-[20px] lg:mx-[15px]">
                         <form className="space-y-[50px]">
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-[40px]">
                                 <div>
-                                    <label htmlFor="name" className="block text-sm font-medium text-white font-inter text-[12px] leading-[20px] font-500">
+                                    <label htmlFor="name" className="block text-sm font-medium text-white font-[inter] text-[12px] leading-[20px] font-500">
                                         First Name
                                     </label>
                                     <input
@@ -66,7 +68,7 @@ export default function ContactSection() {
                                     />
                                 </div>
                                 <div>
-                                    <label htmlFor="text" className="block text-sm font-medium text-white font-inter text-[12px] leading-[20px] font-500">
+                                    <label htmlFor="text" className="block text-sm font-medium text-white font-[inter] text-[12px] leading-[20px] font-500">
                                         Last Name
                                     </label>
                                     <input
@@ -80,7 +82,7 @@ export default function ContactSection() {
                             </div>
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-[40px]">
                                 <div>
-                                    <label htmlFor="email" className="block text-sm font-medium text-white font-inter text-[12px] leading-[20px] font-500">
+                                    <label htmlFor="email" className="block text-sm font-medium text-white font-[inter] text-[12px] leading-[20px] font-500">
                                         Email
                                     </label>
                                     <input
@@ -93,14 +95,14 @@ export default function ContactSection() {
                                 </div>
 
                                 <div>
-                                    <label htmlFor="phone" className="block text-sm font-medium text-white font-inter text-[12px] leading-[20px] font-500">
+                                    <label htmlFor="phone" className="block text-sm font-medium text-white font-[inter] text-[12px] leading-[20px] font-500">
                                         Phone
                                     </label>
                                     <input type="tel" id="phone" name="phone" className="w-full px-1 py-3 text-white border-b border-white outline-none" placeholder="Your phone" />
                                 </div>
                             </div>
                             <div>
-                                <label htmlFor="subject" className="block text-sm font-medium text-white font-inter text-[12px] leading-[20px] font-500">
+                                <label htmlFor="subject" className="block text-sm font-medium text-white font-[inter] text-[12px] leading-[20px] font-500">
                                     Select Subject
                                 </label>
                                 <div className="flex flex-wrap gap-4">
@@ -112,7 +114,7 @@ export default function ContactSection() {
                                             value="general"
                                             className="w-4 h-4 text-white bg-gray-100 border-gray-300 focus:ring-[#ED7D4A]"
                                         />
-                                        <label htmlFor="general" className="ml-3 text-sm text-gray-300 font-inter">
+                                        <label htmlFor="general" className="ml-3 text-sm text-gray-300 font-[inter]">
                                             General Inquiry
                                         </label>
                                     </div>
@@ -124,7 +126,7 @@ export default function ContactSection() {
                                             value="support"
                                             className="w-4 h-4 text-white bg-gray-100 border-gray-300 focus:ring-[#ED7D4A]"
                                         />
-                                        <label htmlFor="support" className="ml-3 text-sm text-gray-300 font-inter">
+                                        <label htmlFor="support" className="ml-3 text-sm text-gray-300 font-[inter]">
                                             Support Request
                                         </label>
                                     </div>
@@ -136,7 +138,7 @@ export default function ContactSection() {
                                             value="partnership"
                                             className="w-4 h-4 text-white bg-gray-100 border-gray-300 focus:ring-[#ED7D4A]"
                                         />
-                                        <label htmlFor="partnership" className="ml-3 text-sm text-white font-inter">
+                                        <label htmlFor="partnership" className="ml-3 text-sm text-white font-[inter]">
                                             Partnership Inquiry
                                         </label>
                                     </div>
@@ -148,14 +150,14 @@ export default function ContactSection() {
                                             value="other"
                                             className="w-4 h-4 text-white bg-gray-100 border-gray-300 focus:ring-[#ED7D4A]"
                                         />
-                                        <label htmlFor="other" className="ml-3 text-sm text-white font-inter">
+                                        <label htmlFor="other" className="ml-3 text-sm text-white font-[inter]">
                                             Other
                                         </label>
                                     </div>
                                 </div>
                             </div>
                             <div>
-                                <label htmlFor="message" className="block text-sm font-medium text-white font-inter text-[12px] leading-[20px] font-500">
+                                <label htmlFor="message" className="block text-sm font-medium text-white font-[inter] text-[12px] leading-[20px] font-500">
                                     Message
                                 </label>
                                 <textarea
